0c540c4304d8359828d4f5bb621cc97a0115c0c2
kasimkkn <kasikkn15@gmail.com>
2024-07-22 16:52:08 +0530

    animation added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,11 +19,11 @@ const Home = () => {
                             </div>
                         </div>
                         <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-                            <h1 className="mb-4" style={{ fontSize: '2.4rem' }}>
+                            <h1 className="mb-4 home-h1">
                                 <span style={{ color: 'var(--primary)', display: 'block' }}>Premium Natural Organic</span>
-                                <span style={{ color: 'black', fontSize: '1.9rem' }}>Honey, Ghee, Jaggery, Salt, </span>
-                                <span style={{ color: 'var(--primary)', fontSize: '2.4rem' }}>&</span>
-                                <span style={{ color: 'black', fontSize: '1.9rem' }}>Brown Sugar</span>
+                                <span style={{ color: 'black'}}>Honey, Ghee, Jaggery, Salt, </span>
+                                <span style={{ color: 'var(--primary)' }}> & </span>
+                                <span style={{ color: 'black'}}>Brown Sugar</span>
                             </h1>
                             <p className="mb-4">Experience the richness of our organic products, meticulously sourced and crafted to
                                 perfection. Our honey, ghee, jaggery, pink or black salt, and brown sugar are all natural,
